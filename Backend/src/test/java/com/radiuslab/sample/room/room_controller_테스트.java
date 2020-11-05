f73752db8c74d6cd980546e9cd765b40518af9e4
juwon <jjw036@gmail.com>
2020-11-05 18:28:22 +0900

    설정 파일 작성

---
+++ Backend/src/test/java/com/radiuslab/sample/room/room_controller_테스트.java
@@ -0,0 +1,13 @@
+package com.radiuslab.sample.room;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = { "spring.datasource.url=jdbc:postgresql://localhost:5432/test" }) // @SpringBootTest에서 다른 db를 사용하겠다
+@AutoConfigureMockMvc
+public class room_controller_테스트 {
+
+}
