67c58f0bd8a9a60a05a04c14648452496220352e
juwon <jjw036@gmail.com>
2020-11-05 16:59:10 +0900

    브렌치 땄쓰예

--- Backend/src/main/java/com/radiuslab/sample/BackendApplication.java
+++ Backend/src/main/java/com/radiuslab/sample/BackendApplication.java
@@ -9,6 +9,7 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 		System.out.println("written by gyuwoon");
+		System.out.println("written by juwon");
 	}
 
 }
