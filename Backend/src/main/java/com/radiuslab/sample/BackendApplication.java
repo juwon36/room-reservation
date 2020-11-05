38bd59fa096938d17d48391bb3054d3a9c523ea1
DESKTOP-3I019BE\gwjung <charminggw@gmail.com>
2020-11-05 16:54:59 +0900

    create 'gyuwoon' branch

--- Backend/src/main/java/com/radiuslab/sample/BackendApplication.java
+++ Backend/src/main/java/com/radiuslab/sample/BackendApplication.java
@@ -8,7 +8,7 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-		System.out.println("gyuwoon");
+		System.out.println("written by gyuwoon");
 	}
 
 }
